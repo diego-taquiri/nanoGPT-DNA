36550cf1ca90742b08f52e377b59499d901d400a
diego-taquiri <diego.taquiri@upch.pe>
2024-10-25 13:00:59 -0500

    Add optimization loop to overfit a single batch; port model and tensors to GPU

--- train_gpt_dna.py
+++ train_gpt_dna.py
@@ -120,7 +120,6 @@ if torch.cuda.is_available():
 elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
-device = "cpu" # OVERRIDE
 
 # Load chromosome 21 from the HG38 genome dataset
 with open('data/chr21.fa') as file:
@@ -138,16 +137,22 @@ tokens = encode(sequence) #tokenize dna sequence
 # get a data batch
 B, T = 4, 32
 buf = torch.tensor(tokens[:B*T + 1])
+buf = buf.to(device)
 x = buf[:-1].view(B, T)
 y = buf[1:].view(B, T)
 
 # get logits
 model = GPT(GPTConfig())
 model.to(device)
-logits, loss = model(x, y)
 
-print(logits.shape)
-print(loss)
+# optimize!
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
 import sys; sys.exit(0)
 
 # prefix tokens
