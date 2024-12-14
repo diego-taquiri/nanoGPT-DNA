6a7cd7d7e98d608e0e1c76405d95093934633179
diego-taquiri <diego.taquiri@upch.pe>
2024-12-14 09:10:54 -0500

    implement model saving and remove text generation code

--- train_gpt_dna.py
+++ train_gpt_dna.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import os
 
 # -----------------------------------------------------------------------------
 
@@ -187,6 +188,11 @@ model.to(device)
 
 # optimize!
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+
+# create the log directory
+log_dir = "log"
+os.makedirs(log_dir, exist_ok=True)
+
 for i in range(50):
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
@@ -195,42 +201,12 @@ for i in range(50):
     loss.backward()
     optimizer.step()
     print(f"step {i}, loss: {loss.item()}")
-import sys; sys.exit(0)
 
-# prefix tokens
-model.eval()
-num_return_sequences = 5
-max_length = 30
-tokens = enc.encode("Hello, I'm a language model,")
-tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
-x = tokens.to(device)
-
-# generate! right now x is (B, T) where B = 5, T = 8
-# set the seed to 42
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
-while x.size(1) < max_length:
-    # forward the model to get the logits
-    with torch.no_grad():
-        logits = model(x) # (B, T, vocab_size)
-        # take the logits at the last position
-        logits = logits[:, -1, :] # (B, vocab_size)
-        # get the probabilities
-        probs = F.softmax(logits, dim=-1)
-        # do top-k sampling of 50 (huggingface pipeline default)
-        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
-        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
-        # select a token from the top-k probabilities
-        # note: multinomial does not demand the input to sum to 1
-        ix = torch.multinomial(topk_probs, 1) # (B, 1)
-        # gather the corresponding indices
-        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
-        # append to the sequence
-        x = torch.cat((x, xcol), dim=1)
-
-# print the generated text
-for i in range(num_return_sequences):
-    tokens = x[i, :max_length].tolist()
-    decoded = enc.decode(tokens)
-    print(">", decoded)
+# Save final model
+out = {
+    'model': model.state_dict(),
+    'config': model.config,
+}
+model_path = os.path.join(log_dir, "model.pt")
+torch.save(out, model_path)
+print(f"saved model to {model_path}")
