7e52564dee05c9fb41764493bd57d4fc9ec097ad
diego-taquiri <diego.taquiri@upch.pe>
2024-12-18 23:15:44 -0500

    perf: optimize model with torch.compile and flash attention (total 6x speedup or ~83%)

--- train_gpt_dna.py
+++ train_gpt_dna.py
@@ -34,11 +34,7 @@ class CausalSelfAttention(nn.Module):
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        # attention (materializes the large (T,T) matrix for all the queries and keys)
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True) # flash attention
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
         # output projection
         y = self.c_proj(y)
@@ -194,8 +190,9 @@ if __name__ == "__main__":
     #torch.set_float32_matmul_precision('high')
 
     # get logits
-    model = GPT(GPTConfig())
+    model = GPT(GPTConfig(vocab_size=6))
     model.to(device)
+    model = torch.compile(model)
 
     # optimize!
     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
